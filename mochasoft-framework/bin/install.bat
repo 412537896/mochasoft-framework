439c00e58aff3769abe429eb84a5b1c32afeae4a
humans1983@gmail.com <humans1983@gmail.com@769f7349-4231-9ef3-c40e-dec8f5b39862>
2013-03-14 12:49:08 +0000

    编译打包修改

--- mochasoft-framework/bin/install.bat
+++ mochasoft-framework/bin/install.bat
@@ -5,19 +5,24 @@ set MAVEN_OPTS=%MAVEN_OPTS% -XX:MaxPermSize=128m
 
 echo [Step 1] Install parent pom.xml to local repository.
 cd %~dp0
-cd ..
+cd ../parent
 call mvn clean install deploy -Dmaven.test.skip=true
 
 echo [Step 2] Install core.jar to local repository.
-cd mochasoft-framework-core
+cd ../mochasoft-framework-core
 call mvn clean install deploy -Dmaven.test.skip=true
 if errorlevel 1 goto error
 
 echo [Step 3] Install web to local repository.
-cd ..
-cd quickstart-web
+cd ../quickstart-web
 call mvn clean install deploy -Dmaven.test.skip=true
 if errorlevel 1 goto error
 
+
+echo [Step 4] run package dist.
 cd ..\bin
+call mvn clean compile -Dmaven.test.skip=true
+if errorlevel 1 goto error
+
+
 pause
